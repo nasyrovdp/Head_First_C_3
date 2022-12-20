767a0dcdd92bacaba0a9787a87efca4213699df3
nasyrovdp <nasyrovdp@yandex.ru>
2022-12-20 16:56:32 +0300

    3x1 done!

--- 3x1/3x1.c
+++ 3x1/3x1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -7,5 +8,24 @@ int main()
     puts("And I do the same with the resulting number until I get to the cycle 4-2-1.");
     puts("When I get to 1 I'll tell you how many steps it took.");
     puts("So now you give me a number:");
+    char number_char[6];
+    int number;
+    int count = 0;
+    scanf("%5s", number_char);
+    number = atoi(number_char);
+    printf("You have given me %i. Let's roll!\n", number);
+    printf("%i >> ", number);
+    while (number != 2) {
+        if (number % 2) {
+            number = (number * 3) + 1;
+        } else {
+            number = number / 2;
+        }
+        count++;
+        printf("%i >> ", number);
+    }
+    printf("1\n");
+    count++;
+    printf("It has taken me %i steps to get to 1.\n", count);
     return 0;
 }
