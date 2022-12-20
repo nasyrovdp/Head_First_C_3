bc4eefd6d456a0e859f90cc3411c7418e95664d7
nasyrovdp <nasyrovdp@yandex.ru>
2022-12-20 14:21:50 +0300

    started 3x1

---
+++ 3x1/3x1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("This is a simple exercise. You give me a number and I do certain math to it.");
+    puts("If the number is even I devide it in 2 and if the number is odd I multiply it by 3 and add 1.");
+    puts("And I do the same with the resulting number until I get to the cycle 4-2-1.");
+    puts("When I get to 1 I'll tell you how many steps it took.");
+    puts("So now you give me a number:");
+    return 0;
+}
