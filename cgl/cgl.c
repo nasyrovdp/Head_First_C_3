a3a5d6b57069e79af672748ca79ff7fce94aa0d1
nasyrovdp <nasyrovdp@yandex.ru>
2022-12-20 12:11:25 +0300

    cgl grid done

--- cgl/cgl.c
+++ cgl/cgl.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    puts()
+    int counth;
+    int countv;
+    for (counth = 1; counth <= 9; counth++) {
+        for (countv = 1; countv <= 9; countv++) {
+            printf(" . ");
+        }
+        printf("\n");
+    }
     return 0;
 }
