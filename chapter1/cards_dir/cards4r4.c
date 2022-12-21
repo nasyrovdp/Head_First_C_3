4d9ad4b552e197297c79b15f7185a0adf24781a4
nasyrovdp <nasyrovdp@yandex.ru>
2022-12-21 16:50:43 +0300

    learned and set to memorising cards4

---
+++ chapter1/cards_dir/cards4r4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char card_name[3];
+    int count;
+    while (card_name[0] != 'X') {
+        puts("Enter card name (of eXit):");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch(card_name[0]) {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;
+            default:
+                val = atoi(card_name);
+                if ((val < 2) || (val > 10)) {
+                    printf("This is not a valid card name. Try again.\n\n");
+                    continue;
+                }
+        }
+        if ((val >= 3) && (val <= 6))
+            count++;
+        else if (val == 10)
+            count--;
+        printf("Current count: %i\n\n", count);
+    }
+    printf("Good bye!\n\n");
+    return 0;
+}
