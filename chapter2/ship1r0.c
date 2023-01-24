4b9035c5e168fabb201eceba71f3f1c5ea93dd33
nasyrovdp <nasyrovdp@yandex.ru>
2023-01-24 22:31:55 +0300

    ship1r0,r1,r2

--- chapter2/ship1r0.c
+++ chapter2/ship1r0.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void go_south_east(int lat, int lon)
+void go_south_east(int *lat, int *lon)
 {
-	lat = lat - 1;
-	lon = lon + 1;
+	*lat = *lat - 1;
+	*lon = *lon + 1;
 }
 
 int main()
 {
 	int latitude = 32;
 	int longitude = -64;
-	go_south_east(latitude, longitude);
+	go_south_east(&latitude, &longitude);
 	printf("Avast! We're now at [%i, %i]\n", latitude, longitude);
 	return 0;
 }
